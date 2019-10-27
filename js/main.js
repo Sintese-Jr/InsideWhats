cb8bd49c25ecd76004fc6dfcfdf40139732bfe2e
LeviUSP <levifernandes@usp.br>
2019-10-27 15:56:41 -0300

    menu pronto!!!!!!

--- js/main.js
+++ js/main.js
@@ -6,3 +6,14 @@ document.querySelector('.menu-abrir').onclick = function() {
 document.querySelector('.menu-fechar').onclick = function() {
     document.documentElement.classList.remove('menu-ativo');
 };
+
+$(function() {
+    $(window).on("scroll", function() {
+        if($(window).scrollTop() > 50) {
+            $(".head").addClass("scrolled");
+        } else {
+            //remove the background property so it comes transparent again (defined in your css)
+           $(".head").removeClass("scrolled");
+        }
+    });
+});
